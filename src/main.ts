497ce24cfa0d295de6824c423356684e8143e122
Chi Tran <chi@bespokify.com>
2023-01-03 16:28:37 +0700

    refactor: refactor code

--- src/main.ts
+++ src/main.ts
@@ -2,19 +2,12 @@ import { ValidationPipe } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import 'reflect-metadata';
-import * as Sentry from '@sentry/node';
-import { ConfigService } from '@nestjs/config';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
   app.useGlobalPipes(new ValidationPipe({ transform: true }));
   app.enableCors();
-
-  const configService = app.get(ConfigService);
-  Sentry.init({
-    dsn: configService.get('SENTRY_DNS'),
-  });
 
   await app.listen(3000);
 }
