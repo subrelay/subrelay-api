327d757f1872c8765478e4fb2ab8645159285581
Chi Tran <ttdiemchi@gmail.com>
2023-05-24 14:36:59 +0700

    fix: migrate user integration

---
+++ src/migration/1684904530759-drop-key-colume.ts
@@ -0,0 +1,14 @@
+import { MigrationInterface, QueryRunner } from "typeorm";
+
+export class DropKeyColume1684904530759 implements MigrationInterface {
+    name = 'DropKeyColume1684904530759'
+
+    public async up(queryRunner: QueryRunner): Promise<void> {
+        await queryRunner.query(`ALTER TABLE "user" DROP COLUMN "key"`);
+    }
+
+    public async down(queryRunner: QueryRunner): Promise<void> {
+        await queryRunner.query(`ALTER TABLE "user" ADD "key" character varying NOT NULL DEFAULT "substring"((sha256(((gen_random_uuid())))), 2)`);
+    }
+
+}
