ddfa3fb1cee0999a4d7d063e3ad515dd37052b83
Chi Tran <ttdiemchi@gmail.com>
2023-05-25 16:31:50 +0700

    refactor: refactor code

--- src/discord/discord.type.ts
+++ src/discord/discord.type.ts
@@ -4,22 +4,10 @@ export class DiscordAuthQueryParams {
   @IsString()
   @IsNotEmpty()
   id: string;
-
-  @IsString()
-  @IsNotEmpty()
-  username: string;
-
-  @IsString()
-  @IsNotEmpty()
-  avatar: string;
 }
 
 export class TelegramAuthQueryParams {
   @IsString()
   @IsNotEmpty()
   id: string;
-
-  @IsString()
-  @IsNotEmpty()
-  username: string;
 }
