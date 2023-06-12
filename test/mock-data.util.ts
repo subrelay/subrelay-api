c43b91c5c39a20b753fb81e0d499c05d8d360b97
Chi Tran <ttdiemchi@gmail.com>
2023-06-12 15:22:06 +0700

    fix: adding new file

---
+++ test/mock-data.util.ts
@@ -0,0 +1,24 @@
+import { ulid } from 'ulid';
+
+export function mockDiscordUser() {
+  return {
+    id: 'discordId',
+    username: 'Discord user',
+    avatar: 'https://example.com/file_1.jpg',
+  };
+}
+
+export function mockTelegramUser() {
+  return {
+    id: 'telegramId',
+    username: 'Telegram user',
+    avatar: 'https://example.com/file_1.jpg',
+  };
+}
+
+export function mockUser() {
+  return {
+    id: ulid(),
+    address: '5Ea3dne7kDTMvSnYCFTFrZsLNputsrg35ZQCaHwuviSYMa3e',
+  };
+}
