2808db18bb738133a575c9dd7ff701a1f94f681f
Chi Tran <ttdiemchi@gmail.com>
2023-06-13 14:54:55 +0700

    fix: workflow integration test

--- test/workflow.e2e-spec.ts
+++ test/workflow.e2e-spec.ts
@@ -15,9 +15,8 @@ import { SubstrateService } from '../src/substrate/substrate.service';
 import { EventEntity } from '../src/event/event.entity';
 import { InternalServerExceptionsFilter } from '../src/common/internal-server-error.filter';
 import { APP_FILTER } from '@nestjs/core';
-import { mockUser } from './mock-data.util';
+import { mockDiscordUser, mockTelegramUser, mockUser } from './mock-data.util';
 import { UserService } from '../src/user/user.service';
-import { ChainEntity } from '../src/chain/chain.entity';
 import { ulid } from 'ulid';
 import { WorkflowModule } from '../src/workflow/workflow.module';
 import { ConfigModule, ConfigService } from '@nestjs/config';
@@ -93,19 +92,68 @@ describe('Task', () => {
     );
     await app.init();
 
+    workflowRepository = moduleRef.get(getRepositoryToken(WorkflowEntity));
     userRepository = moduleRef.get(getRepositoryToken(UserEntity));
-    await userRepository.save([user]);
-
     const eventRepository: Repository<EventEntity> = moduleRef.get(
       getRepositoryToken(EventEntity),
     );
-    event = (await eventRepository.find())[0];
 
-    workflowRepository = moduleRef.get(getRepositoryToken(WorkflowEntity));
+    await userRepository.save([user]);
+    event = (await eventRepository.find())[0];
   });
 
   describe('POST /workflows', () => {
-    it(`Create webhook workflow`, () => {
+    describe('Failed to create workflow', () => {
+      it(`Nonexistent event`, () => {
+        const input = {
+          name: ulid(),
+          tasks: [
+            {
+              name: 'trigger',
+              type: 'trigger',
+              dependOnName: null,
+              config: {
+                eventId: ulid(),
+              },
+            },
+            {
+              name: 'webhook',
+              type: 'webhook',
+              dependOnName: 'trigger',
+              config: {
+                url: 'https://example.com',
+              },
+            },
+          ],
+        };
+        return request(app.getHttpServer())
+          .post('/workflows')
+          .send(input)
+          .expect(400);
+      });
+
+      it(`Invalid input`, () => {
+        const input = {
+          name: ulid(),
+          tasks: [
+            {
+              name: 'trigger',
+              type: 'trigger',
+              dependOnName: null,
+              config: {
+                eventId: event.id,
+              },
+            },
+          ],
+        };
+        return request(app.getHttpServer())
+          .post('/workflows')
+          .send(input)
+          .expect(400);
+      });
+    });
+
+    it(`Webhook workflow`, () => {
       const input = {
         name: ulid(),
         tasks: [
@@ -133,8 +181,153 @@ describe('Task', () => {
         .expect(201)
         .then((res) => {
           expect(res.body.name).toEqual(input.name);
+          expect(res.body.status).toEqual('running');
           workflow = res.body;
         });
+    });
+
+    describe(`Email workflow`, () => {
+      it('Success create workflow', () => {
+        const input = {
+          name: ulid(),
+          tasks: [
+            {
+              name: 'trigger',
+              type: 'trigger',
+              dependOnName: null,
+              config: {
+                eventId: event.id,
+              },
+            },
+            {
+              name: 'email',
+              type: 'email',
+              dependOnName: 'trigger',
+              config: {
+                addresses: ['example@gmail.com'],
+                subjectTemplate: 'Your event has been triggered',
+                bodyTemplate: ' has been sent to  DOT',
+              },
+            },
+          ],
+        };
+        return request(app.getHttpServer())
+          .post('/workflows')
+          .send(input)
+          .expect(201)
+          .then((res) => {
+            expect(res.body.name).toEqual(input.name);
+            expect(res.body.status).toEqual('running');
+            workflow = res.body;
+          });
+      });
+    });
+
+    describe('Discord workflow', () => {
+      const input = {
+        name: ulid(),
+        tasks: [
+          {
+            name: 'trigger',
+            type: 'trigger',
+            dependOnName: null,
+            config: {
+              eventId: '',
+            },
+          },
+          {
+            name: 'discord',
+            type: 'discord',
+            dependOnName: 'trigger',
+            config: {
+              messageTemplate: 'Hello 😃💁',
+            },
+          },
+        ],
+      };
+
+      it(`User does not set up discord connection yet`, () => {
+        input.tasks[0].config.eventId = event.id;
+
+        return request(app.getHttpServer())
+          .post('/workflows')
+          .send(input)
+          .expect(400);
+      });
+
+      it(`Success create workflow`, async () => {
+        input.tasks[0].config.eventId = event.id;
+
+        await userRepository.update(
+          { id: user.id },
+          {
+            integration: {
+              discord: mockDiscordUser(),
+              telegram: null,
+            },
+          },
+        );
+
+        return request(app.getHttpServer())
+          .post('/workflows')
+          .send(input)
+          .expect(201)
+          .then((res) => {
+            expect(res.body.name).toEqual(input.name);
+          });
+      });
+    });
+
+    describe('Telegram workflow', () => {
+      const input = {
+        name: ulid(),
+        tasks: [
+          {
+            name: 'trigger',
+            type: 'trigger',
+            dependOnName: null,
+            config: {
+              eventId: '',
+            },
+          },
+          {
+            name: 'telegram',
+            type: 'telegram',
+            dependOnName: 'trigger',
+            config: {
+              messageTemplate: 'Hello 😃💁',
+            },
+          },
+        ],
+      };
+
+      it(`User does not set up telegram connection yet`, () => {
+        input.tasks[0].config.eventId = event.id;
+
+        return request(app.getHttpServer())
+          .post('/workflows')
+          .send(input)
+          .expect(400);
+      });
+
+      it(`Success create workflow`, async () => {
+        input.tasks[0].config.eventId = event.id;
+
+        await userRepository.update(
+          { id: user.id },
+          {
+            integration: {
+              discord: null,
+              telegram: mockTelegramUser(),
+            },
+          },
+        );
+
+        return request(app.getHttpServer())
+          .post('/workflows')
+          .send(input)
+          .expect(201);
+      });
     });
   });
 
@@ -144,10 +337,69 @@ describe('Task', () => {
         .get('/workflows')
         .expect(200)
         .then((res) => {
-          expect(res.body.total).toBe(1);
-          expect(res.body.workflows).toHaveLength(1);
+          expect(res.body.total).toBe(4);
+          expect(res.body.workflows).toHaveLength(4);
           expect(res.body.workflows[0].id).toEqual(workflow.id);
         });
+    });
+  });
+
+  describe('GET /workflow/{workflow_id}', () => {
+    it(`Get a nonexistence workflow`, () => {
+      return request(app.getHttpServer())
+        .get(`/workflows/${ulid()}`)
+        .expect(404);
+    });
+
+    it(`Get an existing workflow`, () => {
+      return request(app.getHttpServer())
+        .get(`/workflows/${workflow.id}`)
+        .expect(200)
+        .then((res) => {
+          expect(res.body).toEqual(workflow);
+        });
+    });
+  });
+
+  describe('PUT /workflows', () => {
+    it(`Update a nonexistence workflow`, () => {
+      return request(app.getHttpServer())
+        .get(`/workflows/${ulid()}`)
+        .expect(404);
+    });
+
+    it(`Update an existing workflow`, () => {
+      const input = {
+        status: 'pausing',
+        name: ulid(),
+      };
+      return request(app.getHttpServer())
+        .patch(`/workflows/${workflow.id}`)
+        .send(input)
+        .expect(204)
+        .then(() => {
+          return request(app.getHttpServer())
+            .get(`/workflows/${workflow.id}`)
+            .expect(200)
+            .then((res) => {
+              expect(res.body.name).toBe(input.name);
+              expect(res.body.status).toBe(input.status);
+            });
+        });
+    });
+  });
+
+  describe('DELETE /workflow/{workflow_id}', () => {
+    it(`Delete a nonexistence workflow`, () => {
+      return request(app.getHttpServer())
+        .get(`/workflows/${ulid()}`)
+        .expect(404);
+    });
+
+    it(`Delete an existing workflow`, () => {
+      return request(app.getHttpServer())
+        .delete(`/workflows/${workflow.id}`)
+        .expect(204);
     });
   });
 
